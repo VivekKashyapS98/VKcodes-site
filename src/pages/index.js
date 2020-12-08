50dd5baaf5d10cca516f7023c400fc586535422d
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-12-08 12:12:58 +0530

    css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <span className="my-name">Hi! I'm </span><span className="my-name" style={{color: '#21fcab'}}>Vivek.</span><br />
         <span className="my-name" style={{padding:'0 0 0 80px'}}>I'm a </span><span className="my-name" style={{color: '#21fcab'}}>Web Developer..</span>
       </div>
-      <div className="intro-about" style={{width: '350px'}}>
+      <div className="intro-about">
         <p>A highly focused, getting things done in customer oriented manner.</p>
         <p>End result oriented developer and designer, figuring out the best solution to the given software problem.</p>
         <p>Handling the issues wisely by investigating deep into the problem and using resources to it's best.</p>
