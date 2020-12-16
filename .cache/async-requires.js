9680a2c1572a86f87aa726ea01db6b61b33d880d
Vivek Kashyap <vivekyeskashyap@gmail.com>
2020-12-16 15:07:46 +0530

    mobile optimisations

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,6 +3,8 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-project-1-js": () => import("./../../../src/pages/project1.js" /* webpackChunkName: "component---src-pages-project-1-js" */),
+  "component---src-pages-project-2-js": () => import("./../../../src/pages/project2.js" /* webpackChunkName: "component---src-pages-project-2-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,8 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/vivek/Documents/Projects/Showcase Projects/Portfolio/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/home/vivek/Documents/Projects/Showcase Projects/Portfolio/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/home/vivek/Documents/Projects/Showcase Projects/Portfolio/src/pages/index.js"))),
+  "component---src-pages-project-1-js": hot(preferDefault(require("/home/vivek/Documents/Projects/Showcase Projects/Portfolio/src/pages/project1.js"))),
+  "component---src-pages-project-2-js": hot(preferDefault(require("/home/vivek/Documents/Projects/Showcase Projects/Portfolio/src/pages/project2.js")))
 }
 
