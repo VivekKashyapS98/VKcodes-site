265ff38f66319500724b3d22a9fa4a1f4e93beef
Vivek Kashyap <vivekyeskashyap@gmail.com>
2021-06-22 19:30:43 +0530

    TailwindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,40 +12,68 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="text-6xl">
+          Welcome to{" "}
+          <a
+            className="text-blue-500 hover:underline"
+            href="https://nextjs.org"
+          >
+            Next.js!
+          </a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          <code className="text-lg bg-gray-50 px-1 py-1 font-mono">
+            pages/index.js
+          </code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+        <div className="w-2/4 grid grid-cols-2 place-content-center place-items-stretch gap-2 m-5">
+          <a
+            href="https://nextjs.org/docs"
+            className="group flex flex-col items-center px-5 border border-gray-100 text-center rounded-lg hover:border-blue-500  delay-75"
+          >
+            <h2 className="text-left text-2xl mx-2 my-4 group-hover:text-blue-500">
+              Documentation &rarr;
+            </h2>
+            <p className="text-left text-xl mx-2 my-4 max-w-xs group-hover:text-blue-500">
+              Find in-depth information about Next.js features and API.
+            </p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a
+            className="group flex flex-col items-center px-5  border border-gray-100 text-center rounded-lg hover:border-blue-500  delay-75"
+            href="https://nextjs.org/learn"
+          >
+            <h2 className="text-left text-2xl mx-2 my-5 group-hover:text-blue-500">
+              Learn &rarr;
+            </h2>
+            <p className="text-left text-xl mx-2 my-5 group-hover:text-blue-500">
+              Learn about Next.js in an interactive course with quizzes!
+            </p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className="group flex flex-col items-center px-5 border border-gray-100 text-center rounded-lg hover:border-blue-500 delay-75"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2 className="text-left text-2xl mx-2 my-5 group-hover:text-blue-500">
+              Examples &rarr;
+            </h2>
+            <p className="text-left text-xl mx-2 my-5 group-hover:text-blue-500">
+              Discover and deploy boilerplate example Next.js projects.
+            </p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="group flex flex-col items-center px-5 border border-gray-100 text-center rounded-lg hover:border-blue-500 delay-75"
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
+            <h2 className="text-left text-2xl mx-2 my-5 group-hover:text-blue-500">
+              Deploy &rarr;
+            </h2>
+            <p className="text-left text-xl mx-2 my-5 group-hover:text-blue-500">
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
