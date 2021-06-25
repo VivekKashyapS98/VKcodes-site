4d9a809a6f41a6d8e71faa654d498bde4e6581d7
Vivek Kashyap <vivekyeskashyap@gmail.com>
2021-06-25 14:15:45 +0530

    Dark Mode

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,19 @@
 import Button from "./Button";
+import Toggle from "./Toggle";
 
-export default function Layout({ theme, children }: any) {
+export default function Layout({ toggle, theme, children }: any) {
   let mode = theme === "dark" ? theme : " ";
-  let mainBg = theme === "dark" ? "bg-white" : "";
+  let mainBg = theme === "dark" ? "bg-gray-800" : "";
 
   return (
     <div className={`${mode} w-full ${mainBg}`}>
       <div className="container m-auto">
         <div className="h-20 flex flex-row px-4 justify-between items-center">
-          <h1 className="text-3xl font-bold">VKcodes</h1>
-          <Button text="Blog" />
+          <h1 className="text-3xl font-bold dark:text-white">VKcodes</h1>
+          <div className="flex flex-row flex-nowrap">
+            <Toggle toggle={toggle} />
+            <Button text="Blog" />
+          </div>
         </div>
         {children}
       </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,24 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { useState } from "react";
-import { ThemeContext } from "../utils/context";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState("light");
 
-  const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else if (theme === "dark") {
-      setTheme("light");
-    }
+  const toggleTheme = (mode: string) => {
+    setTheme(mode);
   };
 
   return (
-    <Layout>
-      <ThemeContext.Provider value={{ toggleTheme }}>
-        <Component {...pageProps} />
-      </ThemeContext.Provider>
+    <Layout toggle={toggleTheme} theme={theme}>
+      <Component {...pageProps} />
     </Layout>
   );
 }
