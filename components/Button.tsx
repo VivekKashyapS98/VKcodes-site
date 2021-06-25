d2e72dcb6c27c1e3b2dcdf5f4b494661a73d2a4b
Vivek Kashyap <vivekyeskashyap@gmail.com>
2021-06-25 13:06:35 +0530

    Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button(props: { text: string }) {
   return (
-    <button className="my-auto py-2 px-4 bg-purple-500 text-white text-lg text-center shadow-lg rounded-md transition duration-200 ease-in cursor-pointer  hover:shadow-xl active:bg-purple-700">
+    <button className=" my-auto py-2 px-4 bg-purple-500 text-white text-lg font-medium text-center shadow-xl rounded-lg transition duration-200 ease-in cursor-pointer hover:bg-purple-400  hover:shadow-2xl active:bg-purple-700 active:text-white">
       {props.text}
     </button>
   );
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,18 @@
 import Button from "./Button";
 
-export default function Layout({ children }: any) {
+export default function Layout({ theme, children }: any) {
+  let mode = theme === "dark" ? theme : " ";
+  let mainBg = theme === "dark" ? "bg-white" : "";
+
   return (
-    <div className="container m-auto">
-      <div className="h-20 flex flex-row px-4 justify-between items-center">
-        <h1 className="text-3xl font-bold">VKcodes</h1>
-        <Button text="Blog" />
+    <div className={`${mode} w-full ${mainBg}`}>
+      <div className="container m-auto">
+        <div className="h-20 flex flex-row px-4 justify-between items-center">
+          <h1 className="text-3xl font-bold">VKcodes</h1>
+          <Button text="Blog" />
+        </div>
+        {children}
       </div>
-      {children}
     </div>
   );
 }
