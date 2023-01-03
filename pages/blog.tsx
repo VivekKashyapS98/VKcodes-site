746d34b0873667e513e1fce8c6772b70ddaa7281
Vivek Kashyap <vivekyeskashyap@gmail.com>
2023-01-03 16:19:01 +0530

    New Website built using SvelteKit

--- pages/blog.tsx
+++
@@ -1,7 +0,0 @@
-export default function Blog() {
-  return (
-    <div className="mt-0 md:mt-4 w-full min-h-[500px] flex flexrow flex-wrap justify-center items-center p-2 bg-gray-100 rounded-lg md:p-5 md:h-[600px] dark:bg-gray-700">
-      <h1 className="text-xl dark:text-white">Under Development...</h1>
-    </div>
-  );
-}
---
+++ src/components/Skills.svelte
@@ -0,0 +1,277 @@
+<div class="my-10 flex flex-col">
+  <div
+    class="flex flex-row lg:flex-nowrap items-center border-b-2 border-gray-500"
+  >
+    <h3 class="lg:mr-16 pr-4 text-lg lg:text-2xl font-semibold">Frontend:</h3>
+    <div class="flex flex-wrap">
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+      >
+        <img
+          class="logo"
+          src="/logo/javascript.svg"
+          alt="javascript"
+          title="Javascript"
+        />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.typescriptlang.org/"
+        ><img
+          class="logo"
+          src="/logo/typescript.svg"
+          alt="typescript"
+          title="Typescript"
+        />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://reactjs.org/"
+      >
+        <img class="logo" src="/logo/react.svg" alt="react" title="React" />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://nextjs.org/"
+      >
+        <img class="logo" src="/logo/next-js.svg" alt="next" title="Next" />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://svelte.dev/"
+      >
+        <img class="logo" src="/logo/svelte.svg" alt="svelte" title="Svelte" />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://getbootstrap.com/"
+      >
+        <img
+          class="logo"
+          src="/logo/bootstrap.svg"
+          alt="bootstrap"
+          title="Bootstrap"
+        />
+      </a>
+      <a class="logo" target="_blank" rel="noreferrer" href="https://mui.com/">
+        <img class="logo" src="/logo/material-ui.svg" alt="mui" title="MUI" />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://tailwindcss.com/"
+      >
+        <img
+          class="logo"
+          src="/logo/tailwindcss.svg"
+          alt="tailwind"
+          title="TailwindCSS"
+        />
+      </a>
+    </div>
+  </div>
+  <div
+    class="flex flex-row lg:flex-nowrap items-center border-b-2 border-slate-500"
+  >
+    <h3 class="lg:mr-16 pr-6 text-lg lg:text-2xl font-semibold">Backend:</h3>
+    <div class="flex flex-wrap">
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://nodejs.org/"
+      >
+        <img class="logo" src="/logo/node-js.svg" alt="nodejs" title="NodeJS" />
+      </a>
+      <a
+        class="logo bg-slate-100"
+        target="_blank"
+        rel="noreferrer"
+        href="https://expressjs.com/"
+      >
+        <img
+          class="w-[95%]"
+          src="/logo/express.svg"
+          alt="experssjs"
+          title="ExpressJS"
+        />
+      </a>
+      <a class="logo" target="_blank" rel="noreferrer" href="https://go.dev/">
+        <img class="logo" src="/logo/golang.svg" alt="go" title="Go" />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://gin-gonic.com/"
+      >
+        <img class="logo" src="/logo/gin.png" alt="gin" title="Gin (Go)" />
+      </a>
+      <a
+        class="logo"
+        target="_blank"
+        rel="noreferrer"
+        href="https://gofiber.io/"
+      >
+        <img
+          class="logo bg-slate-100"
+          src="/logo/goFiber.svg"
+          alt="gofiber"
+          title="Fiber (Go)"
+        />
+      </a>
+      <a
+        class="logo bg-slate-100"
+        target="_blank"
+        rel="noreferrer"
+        href="https://echo.labstack.com/"
+      >
+        <img
+          class="w-[95%]"
+          src="/logo/echo.svg"
+          alt="echo"
+          title="Echo (Go)"
+        />
+      </a>
+      <a
+        class="logo bg-slate-100"
+        target="_blank"
+        rel="noreferrer"
+        href="https://flask.palletsprojects.com"
+      >
+        <img
+          class="h-[90%]"
+          src="/logo/flask.svg"
+          alt="flask"
+          title="Flask (Python)"
+        />
+      </a>
+      <a class="logo" target="_blank" rel="noreferrer" href="https://actix.rs/">
+        <img
+          class="logo bg-slate-100"
+          src="/logo/actix.png"
+          alt="actix"
+          title="Actix (Rust)"
+        />
+      </a>
+    </div>
+  </div>
+  <div
+    class="flex flex-row lg:flex-nowrap items-center border-b-2 border-slate-500"
+  >
+    <h3 class="lg:mr-16 text-lg lg:text-2xl font-semibold">Databases:</h3>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.mongodb.com/"
+    >
+      <img class="logo" src="/logo/mongodb.svg" alt="mongodb" title="MongoDB" />
+    </a>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.postgresql.org/"
+    >
+      <img
+        class="logo"
+        src="/logo/postgresql.svg"
+        alt="postgresql"
+        title="PostgreSQL"
+      />
+    </a>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://surrealdb.com/"
+    >
+      <img
+        class="logo"
+        src="/logo/surrealdb.svg"
+        alt="surrealdb"
+        title="SurrealDB"
+      />
+    </a>
+  </div>
+  <div
+    class="flex flex-row lg:flex-nowrap items-center border-b-2 border-slate-500"
+  >
+    <h3 class="mr-4 text-lg lg:text-2xl font-semibold">Cross Platform:</h3>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://flutter.dev/"
+    >
+      <img class="logo" src="/logo/flutter.svg" alt="flutter" title="Flutter" />
+    </a>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://reactnative.dev/"
+    >
+      <img
+        class="logo"
+        src="/logo/react.svg"
+        alt="reactnative"
+        title="React Native"
+      />
+    </a>
+  </div>
+  <div
+    class="flex flex-row lg:flex-nowrap items-center border-b-2 border-slate-500"
+  >
+    <h3 class="lg:mr-28 text-lg lg:text-2xl font-semibold">Others:</h3>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://firebase.google.com/"
+    >
+      <img
+        class="logo"
+        src="/logo/firebase.svg"
+        alt="firebase"
+        title="Firebase"
+      />
+    </a>
+    <a
+      class="logo bg-slate-100"
+      target="_blank"
+      rel="noreferrer"
+      href="https://aws.amazon.com/"
+    >
+      <img class="w-[95%]" src="/logo/aws.svg" alt="aws" title="AWS" />
+    </a>
+    <a
+      class="logo"
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.serverless.com/"
+    >
+      <img
+        class="logo"
+        src="/logo/serverless.svg"
+        alt="serverless"
+        title="Serverless Framework"
+      />
+    </a>
+  </div>
+</div>
